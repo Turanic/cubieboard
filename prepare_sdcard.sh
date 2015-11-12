f7e4770ea420d1ba51f51ba8c3a5b7ed6119e677
Arthur d'Avray <arthur.davray@gmail.com>
2015-11-12 15:15:33 +0100

    [SDCARD] dd command fixed

--- prepare_sdcard.sh
+++ prepare_sdcard.sh
@@ -58,9 +58,11 @@ build_image()
     if [ ! -e "$IMG_NAME" ]; then
     # Start dd in background to be able to print its progress waiting it to finish
         dd if=/dev/zero of="$IMG_NAME" bs=1M count="$IMG_SIZE" iflag=fullblock &
+        LAST_PID=$!
     # While dd is still running, show its progress
-        while ps -p $! > /dev/null ; do
-	        kill -USR1 "$!"
+        sleep "$DD_TIMEOUT"
+        while ps -p $LAST_PID > /dev/null ; do
+	        kill -USR1 "$LAST_PID"
 	        sleep "$DD_TIMEOUT"
         done
     fi
